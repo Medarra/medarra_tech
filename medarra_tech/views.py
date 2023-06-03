b5101d6a180f1b76e2ed34b9b6906e414f22b515
Alysse Palmer <apalmer2583@conestogac.on.ca>
2023-06-03 17:46:17 -0400

    Made corrections for pylint errors in views.py

--- medarra_tech/views.py
+++ medarra_tech/views.py
@@ -1,7 +1,16 @@
 # mysite/views.py
 
+"""
+
+Django Views for medarra_tech Project
+
+"""
+
 from django.http import HttpResponse
 
 
 def index(request):
+    """
+    Index Request Function
+    """
     return HttpResponse('Follow along as I continue learning all about web development!')
